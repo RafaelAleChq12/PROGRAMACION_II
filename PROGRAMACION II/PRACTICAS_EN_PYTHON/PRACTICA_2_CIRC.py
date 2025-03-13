b93bddca63e3af1a573c5cb746ee06c600f2af44
RafaelAleChq12 <batallastar@gmail.com>
2025-03-12 22:56:57 -0400

    .

--- PROGRAMACION II/PRACTICAS_EN_PYTHON/PRACTICA_2_CIRC.py
+++ PROGRAMACION II/PRACTICAS_EN_PYTHON/PRACTICA_2_CIRC.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class Punto:
     def __init__(self, x, y):
         self.x = x
@@ -16,8 +18,23 @@ class Circulo:
         return f"Círculo con centro en {self.centro} y radio {self.radio}"
 
     def dibuja_circulo(self):
-        print(f"Dibujando círculo: {self}")
 
+        fig, ax = plt.subplots()
+
+        circulo_plot = plt.Circle((self.centro.x, self.centro.y), self.radio, color='blue', fill=False)
+
+        ax.add_artist(circulo_plot)
+
+        ax.set_xlim(self.centro.x - self.radio - 1, self.centro.x + self.radio + 1)
+        ax.set_ylim(self.centro.y - self.radio - 1, self.centro.y + self.radio + 1)
+
+
+        ax.set_aspect('equal', 'box')
+
+     
+        plt.grid(True)
+        plt.title(f"Círculo con centro en {self.centro} y radio {self.radio}")
+        plt.show()
 
 
 x = float(input("Ingresa la coordenada x del centro: "))
@@ -26,8 +43,9 @@ centro = Punto(x, y)
 
 radio = float(input("Ingresa el radio del círculo: "))
 
-
 circulo = Circulo(centro, radio)
 
 print(circulo)
+
 circulo.dibuja_circulo()
+
